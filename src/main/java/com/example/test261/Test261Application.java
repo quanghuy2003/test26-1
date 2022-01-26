9972aad7d64002dcea6fff01ae0b357c64545d79
quanghuy2002 <hoangqunghuy753@gmail.com>
2022-01-26 09:14:40 +0700

    model

---
+++ src/main/java/com/example/test261/Test261Application.java
@@ -0,0 +1,13 @@
+package com.example.test261;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test261Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test261Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test261/Test261ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test261;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test261ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
